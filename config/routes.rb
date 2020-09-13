48a9a3452fcf6fae57a99912b9a8a82b8e3c5587
mako <coco.jasmine18@icloud.com>
2020-09-13 03:53:18 +0000

    [add]top

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root 'tops#top'
+  get 'tops/about'
   get 'movies/index'
   get 'movies/show'
   get 'movies/new'
   get 'movies/create'
   get 'movies/edit'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
