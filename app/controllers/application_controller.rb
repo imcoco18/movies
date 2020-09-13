be6450a85b943a6f03ef8f88bb1a584b82c5566b
mako <coco.jasmine18@icloud.com>
2020-09-13 09:28:25 +0000

    [add]devise logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # 新規登録後のリダイレクト先をマイページへ
+  # 新規登録後のリダイレクト先を一覧へ
   def after_sign_in_path_for(resource)
     if current_user
       flash[:notice] = "ログインに成功しました"
@@ -11,6 +11,11 @@ class ApplicationController < ActionController::Base
       new_registrations_path
     end
   end
+  # ログアウト後のリダイレクト先
+  def after_sign_out_path_for(resource)
+    root_path
+   end
+
   protected
 
   def configure_permitted_parameters
