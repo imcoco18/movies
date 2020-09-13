9a718380df8fc944e45959bc0f6d741bb0eae341
mako <coco.jasmine18@icloud.com>
2020-09-13 04:49:59 +0000

    [add]index new create css

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,16 +1,28 @@
 class MoviesController < ApplicationController
+
   def index
+    @movies = Movie.all
   end
 
   def show
+    @movie = Movie.find(params[:id])
   end
 
   def new
+    @movie = Movie.new
   end
 
   def create
+    movie = Movie.new(movie_params)
+    movie.save
+    redirect_to movies_path
   end
 
   def edit
+  end
+
+  private
+  def movie_params
+    params.require(:movie).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   root 'tops#top'
-  get 'tops/about'
-  get 'movies/index'
-  get 'movies/show'
-  get 'movies/new'
-  get 'movies/create'
-  get 'movies/edit'
+  get 'about' => 'tops#about'
+  resources :movies
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
